ffa20ec04ac24abb5b6fb09d408bafa3344912ff
Terransky <nodeathking@gmail.com>
2021-09-18 17:24:22 -0700

    made the requested react components and imported them accordingly

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts';
+
+class Main extends React.Component {
+  render() {
+    return (
+      //need to pass data to HornedBeasts somehow
+    )
+  }
+}
