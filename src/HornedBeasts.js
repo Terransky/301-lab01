137b1e7d625e5c5cc7dde283880f256d0ad41656
Terransky <nodeathking@gmail.com>
2021-09-25 18:40:37 -0700

    images passed from data to Main as an array and displayed through HornedBeasts as props

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 class HornedBeasts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fav: 0, };
+  }
+
+
+
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt="alt placeholder" title="title placeholder" />
+        <img src={this.props.image_url} alt={this.props.title} title={this.props.title} />
         <p>{this.props.description}</p>
       </div>
     );
---
+++ src/examples.js
@@ -0,0 +1,71 @@
+
+// Main.js
+import React from 'react';
+import Card from 'react-bootstrap';
+import render from 'react-dom';
+import BeastsArray from './Beast.json';
+
+class Main extends React.Component {
+  render() {
+    let newArr = [];
+    BeastsArray.forEach( (beast) => {
+      newArr.push(<HornedBeast title={beast.title} />);
+    });
+    return (
+      <>
+        {newArr}
+        </>
+    );
+  }
+}
+
+export default Main;
+
+    
+
+
+// code review  })
+//   render() {
+//     return (
+//       BeastArray.map(beast => {
+//         return (
+//           <HornedBeast title={beast.title} />
+//         );
+//       })
+//       render() {
+//         return (
+//           <>
+//           {this.newArray}
+//           </>
+//         );
+//       }
+//     )
+//   }
+// }
+
+// export default Main;
+
+// HornedBeast.js
+import Card from 'react-bootstrap/Card'
+class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favs: 0,
+    };
+  }
+}
+onFav = () => {
+  this.setState({ favs: this.state.favs + 1});
+}
+
+onDislike = () => {
+  this.setstate({ favs: this.state.favs - 1});
+}
+
+render() {
+  return (
+    <div className="m-1" id="container">
+      <Card style
+  )
+}
