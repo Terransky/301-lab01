55572648f737c962e9c11a78ef0290428baae186
Terransky <nodeathking@gmail.com>
2021-09-29 23:29:23 -0700

    in the process of getting form to work

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 class Main extends React.Component {
   constructor(props) {
@@ -12,12 +16,38 @@ class Main extends React.Component {
       beastArr.push(<HornedBeasts 
         title={beast.title} image_url={beast.image_url} description={beast.description} keyword={beast.keyword} horns={beast.horns} />);
     });
+
+    //something like let filteredArr = beastArr.filter(beast => beast.horns === this.state.hornFilter); and display filteredArr to 
     
     return (
       <>
+      <Form>
+        <Form.Row className="align-items-center">
+          <Col xs="auto" className="my-1">
+            <Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+              Preference
+            </Form.Label>
+            <Form.Control
+              as="select"
+              className="mr-sm-2"
+              id="inlineFormCustomSelect"
+              custom
+            >
+              <option value="0">Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Control>
+          </Col>
+          <Col xs="auto" className="my-1">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Form.Row>
+      </Form>
+
         <h2>This is the Main Component</h2>
         <CardGroup>
-        {beastArr}
+          {beastArr}
         </CardGroup>
       </>
     );
